a3bb09b17a7f97e52dc8a7251d145a8c26af96b1
Sebastien Ferre <ferre@irisa.fr>
2019-10-02 23:12:37 +0200

    removing [spec], replaced by [lis]

---
+++ core/lis.ml
@@ -0,0 +1,23 @@
+
+(* kinds of things *)
+(* focus, id, suggestion, constraint, result *)
+
+type freq = { value : int; partial : bool }
+	      
+class virtual ['lis,'focus,'extent,'suggestion] place (lis : 'lis) (focus : 'focus) =
+object
+  method lis = lis
+  method focus = focus
+
+  method virtual eval : ('extent -> unit) -> ('suggestion list -> unit) -> unit
+  method virtual activate : 'suggestion -> ('lis,'focus,'extent,'suggestion) place
+  method virtual abort : unit (* abort any ongoing computation in that place *)
+end
+
+class virtual ['place] lis =
+object
+  method virtual initial_place : 'place
+				    
+end
+
+       
